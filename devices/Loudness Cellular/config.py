f97ec6e507583c4ee01eeffa5b7f05406173ceb2
Rob Faludi <rob@faludi.com>
2024-09-17 17:02:23 -0400

    updates for TagoIO MQTT

--- devices/Loudness Cellular/config.py
+++ devices/Loudness Cellular/config.py
@@ -3,24 +3,29 @@
 __version__ = "1.3.1"
 
 from digi import cloud
-import secrets
 
-MQTT_UPLOAD = False
-MQTT_TOPIC = "loudness"
-UPLOAD_RATE = 6 # upload frequency in seconds
-MQTT_SERVER = "mqtt.tago.io"
-MQTT_PORT = 8883
-MQTT_SSL = True
-MQTT_CLIENT_ID = "sensorlab_client_id"
-
-HTTP_UPLOAD = False
-HTTP_URL = "http://api.tago.io/data"
-HTTP_HEADERS = {"Device-Token": secrets.TAGOIO_TOKEN}
-
-DRM_UPLOAD = True
-STREAM = "xbsl/loudness" #DRM
-DRM_TRANSPORT = cloud.TRANSPORT_UDP # Digi Remote Manager protocol: cloud.TRANSPORT_TCP or cloud.TRANSPORT_UDP
-
+UPLOAD_RATE = 2 # upload frequency in seconds
 MAX_COMMS_FAIL = 10 # number of consecutive communications failures before reset
 INPUT_BUTTON = "D0" # button to shut down cellular component when long-pressed
 STATUS_LED = "D4" # LED output pin for status messages
+
+MQTT_UPLOAD = True
+if MQTT_UPLOAD:
+    MQTT_TOPIC = "loudness"
+    MQTT_SERVER = "mqtt.tago.io"
+    MQTT_PORT = 8883
+    MQTT_SSL = True
+    MQTT_CLIENT_ID = "sensorlab_client_id"
+
+HTTP_UPLOAD = False
+if HTTP_UPLOAD:
+    import secrets
+    HTTP_URL = "http://api.tago.io/data"
+    HTTP_HEADERS = {"Device-Token": secrets.TAGOIO_TOKEN}
+
+DRM_UPLOAD = False
+if DRM_UPLOAD:
+    STREAM = "xbsl/loudness" #DRM
+    DRM_TRANSPORT = cloud.TRANSPORT_UDP # Digi Remote Manager protocol: cloud.TRANSPORT_TCP or cloud.TRANSPORT_UDP
+
+
