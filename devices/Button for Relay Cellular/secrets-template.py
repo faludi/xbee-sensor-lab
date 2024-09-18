9dee0f3667494f4fc46e77ddab937fa741771015
Rob Faludi <rob@faludi.com>
2024-09-18 16:59:25 -0400

    add secrets module templates for non-DRM uploads

--- devices/Button for Relay Cellular/secrets-template.py
+++ devices/Button for Relay Cellular/secrets-template.py
@@ -1,4 +1,12 @@
-# username and password for Digi Remote Manager
-# edit with your authenication information, then rename to "secrets.py"
+### Edit with your authenication information, then rename to "secrets.py" ###
+
+# username and password for MQTT broker
+MQTT_USER = "username_goes_here"
+MQTT_PASSWORD = "password_goes_here"
+
+# Authorization Token for HTTP Upload 
+HTTP_TOKEN = 'http_token_goes_here'
+
+# Username and password for Digi Remote Manager
 DRM_USER = "your_username_here"
 DRM_PASS = "your_password_here"
